ece3f9655160b8477d98fd77348516c60dd1aee1
George Margaritis <gmargaritis@users.noreply.github.com>
2022-10-26 18:09:02 +0300

    feat: add public subnets outputs

--- aws/networking/outputs.tf
+++ aws/networking/outputs.tf
@@ -2,12 +2,20 @@ output "vpc" {
   value = aws_vpc.main.id
 }
 
-output "subnet_primary" {
-  value = aws_subnet.primary.id
+output "subnet_private_primary" {
+  value = aws_subnet.private_primary.id
 }
 
-output "subnet_secondary" {
-  value = aws_subnet.secondary.id
+output "subnet_private_secondary" {
+  value = aws_subnet.private_secondary.id
+}
+
+output "subnet_public_primary" {
+  value = aws_subnet.public_primary.id
+}
+
+output "subnet_public_secondary" {
+  value = aws_subnet.public_secondary.id
 }
 
 output "security_group_vpc" {
