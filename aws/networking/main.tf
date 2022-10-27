4a7a571c89299d686eb53cd13ffef3a9560506f3
George Margaritis <gmargaritis@users.noreply.github.com>
2022-10-27 13:10:22 +0300

    feat: add map_public_ip_on_launch to subnets

--- aws/networking/main.tf
+++ aws/networking/main.tf
@@ -40,31 +40,35 @@ resource "aws_main_route_table_association" "default" {
 }
 
 resource "aws_subnet" "private_primary" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_main_cidr_block, 8, 1)
-  availability_zone = data.aws_availability_zones.available.names[0]
-  tags              = local.tags
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_main_cidr_block, 8, 1)
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = false
+  tags                    = local.tags
 }
 
 resource "aws_subnet" "private_secondary" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_main_cidr_block, 8, 2)
-  availability_zone = data.aws_availability_zones.available.names[1]
-  tags              = local.tags
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_main_cidr_block, 8, 2)
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = false
+  tags                    = local.tags
 }
 
 resource "aws_subnet" "public_primary" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_main_cidr_block, 8, 3)
-  availability_zone = data.aws_availability_zones.available.names[2]
-  tags              = local.tags
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_main_cidr_block, 8, 3)
+  availability_zone       = data.aws_availability_zones.available.names[2]
+  map_public_ip_on_launch = true
+  tags                    = local.tags
 }
 
 resource "aws_subnet" "public_secondary" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_main_cidr_block, 8, 4)
-  availability_zone = data.aws_availability_zones.available.names[3]
-  tags              = local.tags
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_main_cidr_block, 8, 4)
+  availability_zone       = data.aws_availability_zones.available.names[3]
+  map_public_ip_on_launch = true
+  tags                    = local.tags
 }
 
 resource "aws_security_group" "vpc" {
