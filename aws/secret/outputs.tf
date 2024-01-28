0550319d7870975cc7938668f8ac1722e2c33f76
Paris Kasidiaris <paris@withlogic.co>
2024-01-28 22:22:33 +0200

    aws/secret: provider requirements and readme

--- aws/secret/outputs.tf
+++ aws/secret/outputs.tf
@@ -1,7 +1,9 @@
 output "arn" {
-  value = aws_secretsmanager_secret.main.arn
+  value       = aws_secretsmanager_secret.main.arn
+  description = "The ARN of the created secret"
 }
 
 output "policy_arn" {
-  value = aws_iam_policy.main.arn
+  value       = aws_iam_policy.main.arn
+  description = "The ARN for the IAM policy granting read-only access to the secret"
 }
--- aws/secret/variables.tf
+++ aws/secret/variables.tf
@@ -1,7 +1,20 @@
-variable "project" {}
-variable "environment" {}
-variable "domain" {}
+variable "project" {
+  type        = string
+  description = "The name of the project using this secret (e.g. my-app)"
+  nullable    = tru
+}
+variable "environment" {
+  type        = string
+  description = "The environment where this secret is being used (e.g. production)"
+  nullable    = true
+}
+variable "domain" {
+  type        = string
+  description = "The domain name of the project of this secret (e.g. my-app.my-company.local)"
+}
 variable "secrets" {
-  type      = list(map(any))
-  sensitive = true
+  type        = list(map(any))
+  sensitive   = true
+  description = "List of key-value objects to merge and store in a single AWS secret"
+  default     = []
 }
