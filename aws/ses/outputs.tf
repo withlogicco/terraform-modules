d08da27f7125d477efaa22005e2929136c2bae99
Paris Kasidiaris <paris@withlogic.co>
2024-10-24 19:10:07 +0300

    feat (aws/ses): dynamic route53 records and output

--- aws/ses/outputs.tf
+++ aws/ses/outputs.tf
@@ -7,3 +7,8 @@ output "policy_arn" {
   description = "ARN of the SES domain identity policy"
   value       = aws_iam_policy.main.arn
 }
+
+output "validation_dns_records" {
+  description = "DNS records to validate SES"
+  value       = local.validation_dns_records
+}
--- aws/ses/route53.tf
+++ aws/ses/route53.tf
@@ -1,37 +1,10 @@
-resource "aws_route53_record" "domain_amazonses_verification_record" {
-  count   = var.route53_zone != null ? 1 : 0
+resource "aws_route53_record" "ses_validation_record" {
+  count   = var.route53_zone != null ? length(local.validation_dns_records) : 0
   zone_id = var.route53_zone
-  name    = "_amazonses.${var.ses_domain}"
-  type    = "TXT"
-  ttl     = "3600"
-  records = [aws_ses_domain_identity.domain.verification_token]
-}
-
-resource "aws_route53_record" "domain_amazonses_dkim_record" {
-  count   = var.route53_zone != null ? 3 : 0
-  zone_id = var.route53_zone
-  name    = "${element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index)}._domainkey.${var.ses_domain}"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = ["${element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index)}.dkim.amazonses.com"]
-}
-
-resource "aws_route53_record" "domain_amazonses_feedback_mx_record" {
-  count = (var.route53_zone != null && var.mail_from_domain != null) ? 1 : 0
-
-  zone_id = var.route53_zone
-  name    = var.mail_from_domain
-  type    = "MX"
-  records = ["10 feedback-smtp.${var.region}.amazonaws.com"]
-  ttl     = 60
-}
-
-resource "aws_route53_record" "domain_amazonses_feedback_txt_record" {
-  count = (var.route53_zone != null && var.mail_from_domain != null) ? 1 : 0
-
-  zone_id = var.route53_zone
-  name    = var.mail_from_domain
-  type    = "TXT"
-  records = ["v=spf1 include:amazonses.com ~all"]
-  ttl     = 60
+  name    = validation_dns_records_mailfrom_txt[count.index].name
+  type    = validation_dns_records_mailfrom_txt[count.index].type
+  records = [
+    (validation_dns_records_mailfrom_txt[count.index].type == "MX") ?
+    "${validation_dns_records_mailfrom_txt[count.index].priority} ${validation_dns_records_mailfrom_txt[count.index].value}" : validation_dns_records_mailfrom_txt[count.index].value
+  ]
 }
