4c0e87a9dc5835ed814176a788ae89118c4bb231
Paris Kasidiaris <paris@withlogic.co>
2024-10-24 19:24:06 +0300

    fix: local references

--- aws/ses/route53.tf
+++ aws/ses/route53.tf
@@ -1,10 +1,10 @@
 resource "aws_route53_record" "ses_validation_record" {
   count   = var.route53_zone != null ? length(local.validation_dns_records) : 0
   zone_id = var.route53_zone
-  name    = validation_dns_records_mailfrom_txt[count.index].name
-  type    = validation_dns_records_mailfrom_txt[count.index].type
+  name    = local.validation_dns_records_mailfrom_txt[count.index].name
+  type    = local.validation_dns_records_mailfrom_txt[count.index].type
   records = [
-    (validation_dns_records_mailfrom_txt[count.index].type == "MX") ?
-    "${validation_dns_records_mailfrom_txt[count.index].priority} ${validation_dns_records_mailfrom_txt[count.index].value}" : validation_dns_records_mailfrom_txt[count.index].value
+    (local.validation_dns_records_mailfrom_txt[count.index].type == "MX") ?
+    "${local.validation_dns_records_mailfrom_txt[count.index].priority} ${local.validation_dns_records_mailfrom_txt[count.index].value}" : local.validation_dns_records_mailfrom_txt[count.index].value
   ]
 }
