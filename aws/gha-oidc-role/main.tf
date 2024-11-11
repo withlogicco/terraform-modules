9b35e6fdcf74db560ae1f676f633bb5c43afe002
Paris Kasidiaris <paris@withlogic.co>
2024-11-11 12:44:14 +0100

    add(aws): gha iam role

---
+++ aws/gha-oidc-role/main.tf
@@ -0,0 +1,36 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [var.idp_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = "repo:${var.github_repos}:${var.github_branches}"
+    }
+  }
+}
+
+
+resource "aws_iam_role" "main" {
+  name               = var.name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  tags               = var.tags
+}
+
+resource "aws_iam_user_policy_attachment" "main" {
+  count = length(var.policies)
+
+  user       = aws_iam_user.main.name
+  policy_arn = var.policies[count.index]
+}
