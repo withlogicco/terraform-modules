fbc7b0cd789f3eb91b646ee58ce64fe215666c3e
Paris Kasidiaris <paris@withlogic.co>
2024-11-20 16:03:16 +0200

    fix(aws/elasticache-redis): fix subnet group

--- aws/elasticache-redis/main.tf
+++ aws/elasticache-redis/main.tf
@@ -11,4 +11,5 @@ resource "aws_elasticache_cluster" "main" {
   num_cache_nodes      = 1
   parameter_group_name = "default.redis7"
   security_group_ids   = var.security_group_ids
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
 }
